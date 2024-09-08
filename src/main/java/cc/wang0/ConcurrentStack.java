da4a3141385b177729bf7a88116897811ba30760
wang1 <wangyi.u@qq.com>
2024-09-08 21:04:38 +0800

    init:treiber-stack实现

---
+++ src/main/java/cc/wang0/ConcurrentStack.java
@@ -0,0 +1,36 @@
+package cc.wang0;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConcurrentStack<E> {
+    private final AtomicReference<Node<E>> top = new AtomicReference<>();
+
+    public void push(E item) {
+        Node<E> newHead = new Node<E>(item);
+        Node<E> oldHead;
+        do {
+            oldHead = top.get();
+            newHead.next = oldHead;
+        } while (!top.compareAndSet(oldHead, newHead));
+    }
+
+    public E pop() {
+        Node<E> oldHead;
+        Node<E> newHead;
+        do {
+            oldHead = top.get();
+            if (oldHead == null)
+                return null;
+            newHead = oldHead.next;
+        } while (!top.compareAndSet(oldHead, newHead));
+        return oldHead.item;
+    }
+
+    private static class Node <E> {
+        public final E item;
+        public Node<E> next;
+        public Node(E item) {
+            this.item = item;
+        }
+    }
+}
